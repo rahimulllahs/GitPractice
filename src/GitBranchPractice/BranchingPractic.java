6b735bbf779fe4bb735a6dc2b1c0a88de86377a3
rahimullahsiddiqi <rahimullah2009@gmail.com>
2022-11-09 11:27:12 -0500

    push practice

--- src/GitBranchPractice/BranchingPractic.java
+++ src/GitBranchPractice/BranchingPractic.java
@@ -7,5 +7,8 @@ public class BranchingPractic {
 		
 		System.out.println("i learn this or i will ");
 	}
-
+   public void pushPractice () {
+	   
+	   System.out.print("hope this time works");
+   }
 }
